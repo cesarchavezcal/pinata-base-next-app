673005f970e1dfa11436aa7a716842c759f4b6c0
César Chávez <cesarchavezcal@gmail.com>
2024-02-04 14:15:38 -0600

    fix(navbar): test client side solution
    fix #3

--- src/components/globals/navbar/navbar.tsx
+++ src/components/globals/navbar/navbar.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,26 +9,26 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { auth } from '@/lib/auth/auth';
+
+import { SessionProvider, useSession } from 'next-auth/react';
 import { SignOutItem } from './logout-item';
 
 export interface NavbarProps {}
 
-async function UserMenu() {
-  const session = await auth();
-
+function UserMenu() {
+  const { data: session, status } = useSession();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="flex gap-4 border rounded-full items-center pl-4 pr-1 py-1">
-          <section className="grid justify-items-end">
-            <p className="text-sm w-fit text-right">{session?.user.name}</p>
-            <p className="text-xs w-fit text-right">{session?.user.email}</p>
-          </section>
+        <button className="flex gap-2 border rounded-full items-center pl-2 pr-3 py-1">
           <Avatar className="border">
             <AvatarImage src={session?.user.image as string} />
             <AvatarFallback>{session?.user.name?.substring(0, 2)}</AvatarFallback>
           </Avatar>
+          <section className="grid gap-y-1">
+            <p className="text-sm w-fit text-right">{session?.user.name}</p>
+            <Badge variant="secondary">{status}</Badge>
+          </section>
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
@@ -43,13 +45,15 @@ async function UserMenu() {
   );
 }
 
-export async function Navbar() {
+export function Navbar() {
   return (
     <nav className="w-full p-4 flex justify-between bg-background border sticky top-0 z-10 items-center">
       <span className="text-xl font-semibold text tracking-wider">
         <i className="text-2xl mr-2">🪅</i> Piñata
       </span>
-      <UserMenu />
+      <SessionProvider>
+        <UserMenu />
+      </SessionProvider>
     </nav>
   );
 }
